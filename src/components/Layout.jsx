f7edeb7277c36825836c00e4e24a1115b2450cf6
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-05-16 22:14:42 +0100

    change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,25 @@
 import Sidebar from "./Sidebar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Others from "../pages/Others";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
+  const [side, setSide] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname !== "/" ) {
+      setSide(false);
+    }
+  }, [location.pathname]);
   return (
     <div className="flex flex-col lg:flex-row h-screen">
-      <Sidebar />
+      {side && <Sidebar />}
 
       <div className="w-full bg-[#F1F7FF] overflow-y-scroll">
-        <Header/>
+        {side && <Header />}
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="*" element={<Others />} />
--- src/pages/Others.jsx
+++ src/pages/Others.jsx
@@ -1,9 +1,9 @@
-import { HandIcon } from "@radix-ui/react-icons"
+import { FaceIcon } from "@radix-ui/react-icons"
 const Others = () => {
   return (
-    <div className="flex items-center justify-center pt-10 gap-4 text-2xl">
-      <p>Hello there!</p>
-      <HandIcon className="text-orange-300 animate-bounce" />
+    <div className="flex items-center justify-center pt-10 gap-4 text-2xl lg:text-4xl">
+      <p>Page not Found!</p>
+      <FaceIcon className="text-orange-300 animate-bounce" />
     </div>
   )
 }
